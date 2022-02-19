9541257064e935b949b2614d6dd618dba063625c
FanzBotz89 <faisalalbono@gmail.com>
2022-02-19 14:58:56 +0700

    v2

---
+++ message/info1.js
@@ -0,0 +1,34 @@
+exports.bot = (gender,fake,nomerOwner)=> {
+return`
+*╭─▸*「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭 」
+*│*
+*│▸* Nama : ${fake}
+*│▸* Gender : ${gender}
+*│▸* Hoby : JB ZeilStore
+*│▸* Owner : ${nomerOwner}
+*│▸* Tgl pembuatan : 25-07-2021
+*│▸* Tgl release : 29-09-2021
+*│▸* Image : Gawr Gura
+*│▸* Voice : Nina kawai
+*│*
+*│▸* SCRIPT BY
+*│*   ZeilBotz
+*│*   AlyaaXyz?
+*│*   Yogi PW
+*│*   Hexagon
+*│*   Dttaz
+*│*
+*│▸* FEATURE  BY  
+*│*   ZeilBotz
+*│*   AlyaaXyz?
+*│*   X-Dev Team
+*│*   Resta Gamteng
+*│*   Zeeone Ofc
+*│*   X-Dev Team
+*│*   Yudha perdana
+*│*   Xchilds
+*│*
+*╰────────────[*`
+}
+
+
---
+++ message/sewabot.js
@@ -0,0 +1,53 @@
+
+exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
+return `${ucapanWaktu} kak ${pushname}
+Berikut adalah list harga untuk sewa botz
+
+*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*┴*
+*│▸* Seminggu : 5K         
+*│▸* 1 Bulan = 10K        
+*│▸* permanen = 20K          
+*|-* Run Bot = 30K + Jadi Owner             
+*┬*
+*╰────────────[*
+
+*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
+*┴*
+*│▸* Shopee : ${settings.Shopee}
+*│▸* Gopay : ${settings.Gopay}
+*│▸* Dana : ${settings.Dana}
+*│▸* Ovo : ${settings.Ovo}
+*┬*
+*╰────────────[*
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam tapi kadang 
+juga mati klo lgi ada error 
+atau lgi perbaikan bug.
+
+Kalo mau sewa bisa chat
+owner Bot,
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
